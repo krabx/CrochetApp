e3b3987a36ecab015796dcba0c994b3e6aa96823
Bogdan Radchenko <inroph@mail.ru>
2023-07-08 16:06:27 +0300

    work with UIScrollView

---
+++ CrochetApp/ViewControllers/MySchemasViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MySchemasViewController.swift
+//  CrochetApp
+//
+//  Created by Богдан Радченко on 08.07.2023.
+//
+
+import UIKit
+
+class MySchemasViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //scrollView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+    }
+
+}
